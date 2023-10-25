f3db43c7afbc4ea4565f1cedfe1195ba15ee1482
HananehMirSoltani <hananeh.mir04@gmail.com>
2023-10-25 19:30:37 +0100

    Add prime calculator

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+def is_prime (number):
+    if number<=1:
+        return False
+    elif number %2 == 0:
+        return False
+    else:
+        for i in range (3, int(number/2)+1):
+            if number%1==0:
+                return False
+            else:
+                return True
+    
 
 def primes(number_of_primes):
     list = []
+    num= 2
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+            num +=1
     return list
+
+
+
