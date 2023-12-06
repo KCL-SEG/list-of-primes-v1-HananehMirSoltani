5d0594355d3284c184e4bdb4450d2755eff35f0d
HananehMirSoltani <hananeh.mir04@gmail.com>
2023-12-06 22:10:59 +0000

    Add primes.py

--- primes.py
+++ primes.py
@@ -1,26 +1,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-def is_prime (number):
-    if number<=1:
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
         return False
-    elif number %2 == 0:
-        return False
-    else:
-        for i in range (3, int(number/2)+1):
-            if number%1==0:
-                return False
-            else:
-                return True
-    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
-    list = []
-    num= 2
-    while len(list) < number_of_primes:
+    """Return a list of the first number_of_primes prime numbers."""
+    prime_list = []
+    num = 2  # Start checking for prime from 2
+    while len(prime_list) < number_of_primes:
         if is_prime(num):
-            list.append(num)
-            num +=1
-    return list
-
+            prime_list.append(num)
+        num += 1
+    return prime_list
 
 
